b3cb1959e777539add9d6b2ad40718e2494f7a9f
Wade Gulbrandsen <wade.gulbrandsen@gmail.com>
2026-02-09 17:59:38 -0500

    Process videos for faststart

--- handler_upload_video.go
+++ handler_upload_video.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"bytes"
 	"crypto/rand"
 	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"io"
+	"log"
 	"mime"
 	"net/http"
 	"os"
+	"os/exec"
+	"path"
 	"strings"
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
@@ -76,28 +81,65 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 	tmpFile, err := os.CreateTemp("", "tubely-upload.mp4")
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Unable to save video", err)
+		return
 	}
 	defer os.Remove(tmpFile.Name())
 	defer tmpFile.Close()
 	_, err = io.Copy(tmpFile, file)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Unable to save video", err)
+		return
 	}
 	_, err = tmpFile.Seek(0, io.SeekStart)
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Unable to save video", err)
+		return
 	}
+
+	// Get Aspect Ratio and use that to set the prefix
+	aspectRatio, err := getVideoAspectRatio(tmpFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to determine aspect ratio", err)
+		return
+	}
+	var prefix string
+	switch aspectRatio {
+	case "16:9":
+		prefix = "landscape"
+	case "9:16":
+		prefix = "portrait"
+	default:
+		prefix = "other"
+	}
+
+	// Process the video for faststart
+	processedPath, err := processVideoForFastStart(tmpFile.Name())
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to process video", err)
+		return
+	}
+	proccessedFile, err := os.Open(processedPath)
+	if err != nil {
+		respondWithError(w, http.StatusInternalServerError, "Unable to process video", err)
+		return
+	}
+
+	// Set the key to use in S3
 	videoKeyBytes := make([]byte, 32)
 	rand.Read(videoKeyBytes)
 	videoKey := base64.RawURLEncoding.EncodeToString(videoKeyBytes) + "." + ext
+	videoKey = path.Join(prefix, videoKey)
+
+	// Upload the video to S3
 	_, err = cfg.s3Client.PutObject(r.Context(), &s3.PutObjectInput{
 		Bucket:      &cfg.s3Bucket,
 		Key:         &videoKey,
-		Body:        tmpFile,
+		Body:        proccessedFile,
 		ContentType: &mediaType,
 	})
 	if err != nil {
 		respondWithError(w, http.StatusInternalServerError, "Unable to save video", err)
+		return
 	}
 
 	// Update the video URL in the database
@@ -108,4 +150,74 @@ func (cfg *apiConfig) handlerUploadVideo(w http.ResponseWriter, r *http.Request)
 		return
 	}
 	respondWithJSON(w, http.StatusOK, video)
+}
+
+func getVideoAspectRatio(filePath string) (string, error) {
+	type stream struct {
+		Index              int    `json:"index"`
+		CodecType          string `json:"codec_type"`
+		Width              int    `json:"width"`
+		Height             int    `json:"height"`
+		DisplayAspectRatio string `json:"display_aspect_ratio"`
+	}
+	type ffprobe struct {
+		Streams []stream `json:"streams"`
+	}
+
+	cmd := exec.Command(
+		"ffprobe",
+		"-v", "error",
+		"-print_format", "json",
+		"-show_streams",
+		filePath,
+	)
+	log.Printf("Running %v", cmd.String())
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("Unable to probe video: %v", err)
+	}
+	out_json := ffprobe{}
+	err = json.Unmarshal(out.Bytes(), &out_json)
+	if err != nil {
+		return "", fmt.Errorf("Unable to read probe data: %v", err)
+	}
+	var first_video *stream
+	for _, stream := range out_json.Streams {
+		if stream.CodecType == "video" {
+			first_video = &stream
+			break
+		}
+	}
+	if first_video == nil {
+		return "", err
+	}
+	width := first_video.Width
+	height := first_video.Height
+	switch {
+	case width == 16*height/9:
+		return "16:9", nil
+	case height == 16*height/9:
+		return "9:16", nil
+	default:
+		return "other", nil
+	}
+}
+
+func processVideoForFastStart(filePath string) (string, error) {
+	outputPath := filePath + ".processing"
+	cmd := exec.Command(
+		"ffmpeg",
+		"-i", filePath,
+		"-c", "copy",
+		"-movflags", "faststart",
+		"-f", "mp4", outputPath,
+	)
+	log.Printf("Running %v", cmd.String())
+	err := cmd.Run()
+	if err != nil {
+		return "", fmt.Errorf("Unable to process video: %v", err)
+	}
+	return outputPath, nil
 }
